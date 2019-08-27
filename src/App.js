e28d097c5a8c13b390cb86beb8525b573c3a1c6d
Matt Richards <matt@kreative.co.uk>
2019-08-27 22:50:04 +0100

    Initialises Material UI.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import CssBaseline from '@material-ui/core/CssBaseline'
 import Home from './components/Home/Home'
 import Registration from './components/Registration/Registration'
 
 function App() {
   return (
-    <Router>
-      <Route exact path="/" component={Home} />
-      <Route path="/registration" component={Registration} />
-    </Router>
+    <>
+      <CssBaseline />
+      <Router>
+        <Route exact path="/" component={Home} />
+        <Route path="/registration" component={Registration} />
+      </Router>
+    </>
   )
 }
 
