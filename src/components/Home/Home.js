fa5f36581d571771fbf34156ddea2813a3343530
Matt Richards <matt@kreative.co.uk>
2019-08-27 22:50:26 +0100

    Adds MUI components to home.

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,15 +1,52 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import { withStyles } from '@material-ui/core/styles'
+
+import Paper from '@material-ui/core/Paper'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
+
 import './Home.css'
 
-const Home = () => {
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+    maxWidth: 500,
+  },
+})
+
+const ListItemLink = ({ primary, secondary, to }) => (
+  <ListItem button component={Link} to={to}>
+    <ListItemText primary={primary} secondary={secondary} />
+  </ListItem>
+)
+
+ListItemLink.propTypes = {
+  primary: PropTypes.node.isRequired,
+  secondary: PropTypes.node,
+  to: PropTypes.string.isRequired,
+};
+
+const Home = ({ classes }) => {
   return (
-    <ul>
-      <li>
-        <Link to="/registration">Registration form</Link>
-      </li>
-    </ul>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <List component="nav">
+          <ListItemLink to="/registration" primary="Registration form" secondary="A description about registration form" />
+        </List>
+      </Paper>
+    </div>
   )
 }
 
-export default Home
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(Home)
