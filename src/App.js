2af76097f63903fdae28818fcaeb8203fafd743c
Matt Richards <matt@kreative.co.uk>
2019-08-28 13:06:17 +0100

    Adds global top navigation.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
-import CssBaseline from '@material-ui/core/CssBaseline'
-import Home from './components/Home/Home'
-import Registration from './components/Registration/Registration'
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import Home from "./components/Home/Home";
+import Registration from "./components/Registration/Registration";
 
-function App() {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    marginBottom: theme.spacing(3)
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const App = props => {
+  const classes = useStyles();
   return (
     <>
       <CssBaseline />
+      <div className={classes.root}>
+        <AppBar position="sticky">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Material UI with Formik
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
       <Router>
         <Route exact path="/" component={Home} />
         <Route path="/registration" component={Registration} />
       </Router>
     </>
-  )
-}
+  );
+};
 
 export default App;
