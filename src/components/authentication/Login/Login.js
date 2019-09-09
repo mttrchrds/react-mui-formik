0fac91652721ec2ac25f732552b8edde866d9079
Matt Richards <matt@kreative.co.uk>
2019-09-09 23:38:43 +0100

    Adds basic Yup validation to login form.

--- src/components/authentication/Login/Login.js
+++ src/components/authentication/Login/Login.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Link, Redirect } from 'react-router-dom'
+import { Formik } from 'formik'
+import * as Yup from 'yup'
 
 import { withStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
@@ -29,24 +31,24 @@ const styles = theme => ({
   linkForgotPassword: {
     color: theme.palette.primary.main,
   },
-  leftIcon: {
-    marginRight: theme.spacing.unit,
+  label: {
+    fontSize: 'inherit',
   },
 })
 
+const validationSchema = Yup.object().shape({
+  email: Yup.string()
+    .email('Must be a valid email address')
+    .required('Enter an email address'),
+  password: Yup.string()
+    .min(8, 'Must be at least 8 characters')
+    .max(64, 'Cannot be longer than 64 characters')
+    .required('Enter a password'),
+})
+
 const Login = ({ classes }) => {
-  const [fieldEmail, setEmail] = useState('')
-  const [fieldPassword, setPassword] = useState('')
   const [fieldSaveLogin, setSaveLogin] = useState(false)
   const [redirectRegistration, setRedirectRegistration] = useState(false)
-
-  const handleChangeEmail = event => {
-    setEmail(event.target.value)
-  }
-
-  const handleChangePassword = event => {
-    setPassword(event.target.value)
-  }
 
   const handleChangeSaveLogin = event => {
     setSaveLogin(event.target.checked)
@@ -68,63 +70,89 @@ const Login = ({ classes }) => {
           <Typography align="center" variant="h5" className={classes.formTitle}>
             Login to your account
           </Typography>
-          <form noValidate autoComplete="off">
-            <TextField
-              id="email"
-              label="Email address"
-              className={classes.textField}
-              value={fieldEmail}
-              onChange={handleChangeEmail}
-              margin="normal"
-              fullWidth
-              required
-              variant="outlined"
-            />
-            <TextField
-              id="password"
-              label="Password"
-              className={classes.textField}
-              value={fieldPassword}
-              onChange={handleChangePassword}
-              margin="normal"
-              fullWidth
-              required
-              variant="outlined"
-            />
-            <FormGroup row className={classes.rememberRow}>
-              <FormControlLabel
-                control={<Checkbox color="primary" checked={fieldSaveLogin} onChange={handleChangeSaveLogin} />}
-                label="Remember you"
-              />
-              <span className="link-forgot-password">
-                <Link to="/forgot-password" className={classes.linkForgotPassword}>
-                  Forgot your password?
-                </Link>
-              </span>
-            </FormGroup>
-            <Button variant="contained" color="primary" fullWidth size="large" type="submit">
-              Login
-            </Button>
-            <div className="divider-container">
-              <Divider label="or continue with" />
-            </div>
-            <div className="form-row-split">
-              <Button variant="outlined" size="large" className={classes.margin} fullWidth>
-                <img src={process.env.PUBLIC_URL + 'facebook-logo.svg'} alt="Facebook logo" className="icon icon--facebook" />
-                Facebook
-              </Button>
-              <Button variant="outlined" size="large" className={classes.margin} fullWidth>
-                <img src={process.env.PUBLIC_URL + 'google-logo.svg'} alt="Google logo" className="icon icon--google" />
-                Google
-              </Button>
-            </div>
-            <div className="divider-container">
-              <Divider label="don't have an account?" />
-            </div>
-            <Button variant="contained" color="secondary" fullWidth size="large" type="button" onClick={handleClickJoin}>
-              Create account
-            </Button>
-          </form>
+          <Formik
+            initialValues={{ email: '', password: '' }}
+            validationSchema={validationSchema}
+            onSubmit={(values, { setSubmitting, resetForm }) => {
+              console.log('Submitted values', values)
+              setSubmitting(true)
+              setTimeout(() => {
+                // Mimic half a second wait
+                resetForm()
+                setSubmitting(false)
+              }, 500)
+            }}
+            validateOnChange={false}>
+            {({ values, errors, touched, handleChange, handleSubmit, isSubmitting }) => (
+              <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+                <TextField
+                  id="email"
+                  label="Email address"
+                  className={classes.textField}
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                  name="email"
+                  onChange={handleChange}
+                  value={values.email}
+                  helperText={errors.email}
+                  error={errors.email ? true : false}
+                />
+                <TextField
+                  id="password"
+                  label="Password"
+                  className={classes.textField}
+                  type="password"
+                  margin="normal"
+                  fullWidth
+                  required
+                  variant="outlined"
+                  name="password"
+                  onChange={handleChange}
+                  value={values.password}
+                  helperText={errors.password}
+                  error={errors.password ? true : false}
+                />
+                <FormGroup row className={classes.rememberRow}>
+                  <FormControlLabel
+                    control={<Checkbox color="primary" checked={fieldSaveLogin} onChange={handleChangeSaveLogin} />}
+                    label="Remember you"
+                    classes={{
+                      label: classes.label,
+                    }}
+                  />
+                  <span className="link-forgot-password">
+                    <Link to="/forgot-password" className={classes.linkForgotPassword}>
+                      Forgot your password?
+                    </Link>
+                  </span>
+                </FormGroup>
+                <Button variant="contained" color="primary" fullWidth size="large" type="submit" disabled={isSubmitting}>
+                  Login
+                </Button>
+                <div className="divider-container">
+                  <Divider label="or continue with" />
+                </div>
+                <div className="form-row-split">
+                  <Button variant="outlined" size="large" className={classes.margin} fullWidth>
+                    <img src={process.env.PUBLIC_URL + 'facebook-logo.svg'} alt="Facebook logo" className="icon icon--facebook" />
+                    Facebook
+                  </Button>
+                  <Button variant="outlined" size="large" className={classes.margin} fullWidth>
+                    <img src={process.env.PUBLIC_URL + 'google-logo.svg'} alt="Google logo" className="icon icon--google" />
+                    Google
+                  </Button>
+                </div>
+                <div className="divider-container">
+                  <Divider label="don't have an account?" />
+                </div>
+                <Button variant="contained" color="secondary" fullWidth size="large" type="button" onClick={handleClickJoin}>
+                  Create account
+                </Button>
+              </form>
+            )}
+          </Formik>
         </Paper>
       </div>
     </div>
