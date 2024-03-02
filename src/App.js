c37ced5684562d9eee03205f50954b93cca251a2
Matt Richards <me@mttrchrds.com>
2024-03-02 18:13:34 +0000

    Fully removes routing.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Toolbar from '@material-ui/core/Toolbar'
 import IconButton from '@material-ui/core/IconButton'
 import Typography from '@material-ui/core/Typography'
 import MenuIcon from '@material-ui/icons/Menu'
-import Home from './components/Home/Home'
 import Login from './components/authentication/Login/Login'
 
 const useStyles = makeStyles(theme => ({
